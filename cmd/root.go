8e9f5b9ffa0e10ca91cb189d388f55da12001f42
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-11-24 13:54:40 +0000

    gonnam merge

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,9 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
-
-	"github.com/lordofthemind/gollama/configs"
-	"github.com/lordofthemind/gollama/utils"
 )
 
 var rootCmd = &cobra.Command{
@@ -20,12 +16,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// If no subcommand is specified, default to running the chat command
-		if len(args) == 0 {
-			chatCmd.Run(cmd, args)
-		} else {
-			fmt.Println("Command not recognized. Use gollama --help for a list of commands.")
-		}
+
 	},
 }
 
@@ -40,32 +31,4 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	rootCmd.AddCommand(chatCmd) // Add chat command as a subcommand
-	rootCmd.AddCommand(cnfgCmd) // Add other commands here
-	rootCmd.AddCommand(initCmd)
-	rootCmd.AddCommand(updtCmd)
-
-	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
-		// Ensure Ollama is installed
-		if !utils.CheckOllamaInstallation() {
-			os.Exit(1)
-		}
-
-		// Ensure Ollama is running
-		if !utils.CheckAndStartOllama() {
-			os.Exit(1)
-		}
-
-		// Ensure gollama.yaml exists and load configuration
-		config, configPath, err := configs.LoadGlobalConfig()
-		if err != nil {
-			fmt.Println("Error loading configuration:", err)
-			os.Exit(1)
-		}
-
-		if !config.SetupCompleted {
-			fmt.Println("Configuration setup required. Starting setup...")
-			initiateGlobalConfigurationSetup(&config, configPath)
-		}
-	}
 }
